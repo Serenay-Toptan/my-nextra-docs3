0958ad48350b0644be283370860c8f062e167fb9
SerenayToptan <serenaytoptan12@gmail.com>
2025-07-08 20:30:04 +0300

    Hide sidebar completely and update navbar style

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,17 @@
 import Logo from './components/Logo'
 import meta from './meta.json'
 
-
 const config = {
   logo: function LogoWrapper() {
-    return <Logo />;
+    return (
+      <div className="custom-logo">
+        <Logo />
+      </div>
+    )
   },
-  
-  
-  search: false, 
-  
 
- 
+  search: false,
+  sidebar: false,
 
   project: {
     link: 'https://github.com/Serenay-Toptan/my-nextra-docs3',
@@ -26,7 +26,11 @@ const config = {
   banner: {
     key: 'new-release',
     text: (
-      <a href="https://v0-open-in-v0-chi-ten.vercel.app" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://v0-open-in-v0-chi-ten.vercel.app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         🎉 Check out the new version of our docs site!
       </a>
     ),
@@ -49,6 +53,20 @@ const config = {
   },
 
   primaryHue: 330,
+
+  navbar: {
+    extraContent: (
+      <div className="custom-menu">
+        <ul className="custom-menu-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    ),
+  },
 }
 
 export default config
